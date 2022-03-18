284b55551149163fcf0e381962689e2b71fffb97
Ashutosh Matai <mataiashu@gmail.com>
2022-03-18 18:37:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  let ip=req.headers['x-forwarded-for'], lang= req.headers['accept-language'], soft = req.headers['user-agent'];
+  res.json({ipaddress: ip, language: lang, software: soft});
 });
 
 
